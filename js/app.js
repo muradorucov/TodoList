2361447e2b1f24ca463f4731a2f62d68f2804235
muradorucov <orucowmurad@gmail.com>
2022-04-16 01:31:35 +0400

    TodoList Update

--- js/app.js
+++ js/app.js
@@ -1,27 +1,31 @@
 let btnAdd = document.querySelector('.data-add');
 let inputData = document.querySelector('.input');
 let list = document.querySelector('ul');
+let listBorder = document.querySelector('.list')
 let deleteData = document.querySelector('.delete');
 let listArr = [];
 btnAdd.addEventListener("click", () => {
     listArr.push(inputData.value);
     let space = "";
     listArr.forEach((element, index) => {
-        space += ` <li>${element} <img class="deleteBtn" onclick="del(${index})" src="./icons/icon2.png" /></li>`
+        space += ` <li>${element} <img class="deleteBtn" onclick="del(${index})" src="./icons/whitedelete.png" /></li>`
     })
     list.innerHTML = space;
+    listBorder.style.border = '1px solid #C4C4C4';
+    listBorder.style.margin = '20px 0';
 });
 
 function del(index) {
     listArr.splice(index, 1)
     let space = "";
     listArr.forEach((element, index) => {
-        space += ` <li>${element} <img class="deleteBtn" onclick="del(${index})" src="./icons/icon2.png" /></li>`
+        space += ` <li>${element} <img class="deleteBtn" onclick="del(${index})" src="./icons/whitedelete.png" /></li>`
     })
     list.innerHTML = space;
 
     if (space == "") {
-        list.style.border = "none"
+        listBorder.style.border = "none";
+        listBorder.style.margin = '0';
     }
 };
 
